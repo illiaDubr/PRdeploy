55f4a08c513cc7f446b571f856d40c13f0c6776a
Illia <illia.dubrovin@gmail.com>
2024-12-17 20:20:09 +0200

    a16

--- frontend/src/components/AppRouter.jsx
+++ frontend/src/components/AppRouter.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import {authRoutes, publicRoutes} from "../routes.js";
 import NotFoundPage from "../pages/platform/NotFoundPage.jsx";
+import {Context} from "../api/store/storeContext.js";
 
 
 const AppRouter = () => {
-    const isAt = true
+    const {store} = React.useContext(Context);
+
+    console.log(store);
+
     return (
         <Routes>
-            {isAt === true && authRoutes.map(({path, Component}) =>
+            {store.isAuth && authRoutes.map(({path, Component}) =>
                 <Route key={path} path={path} element={<Component />} />
             )}
             {publicRoutes.map(({path, Component}) =>
