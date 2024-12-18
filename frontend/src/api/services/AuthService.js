72e31fc78621000994e030c70a31f4f87821e8d6
Illia <illia.dubrovin@gmail.com>
2024-12-18 23:43:01 +0200

    s24

--- frontend/src/api/services/AuthService.js
+++ frontend/src/api/services/AuthService.js
@@ -19,7 +19,7 @@ export default class AuthService {
             await this.getCsrfToken();
 
             // Выполняем запрос на логин
-            const response = await $api.post('/login', { 
+            const response = await $api.post('/api/login', { 
                 email, 
                 password 
             }, {
