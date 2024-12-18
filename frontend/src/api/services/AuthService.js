601e30b371604ea36cf70744c62aa6064e31fac2
Illia <illia.dubrovin@gmail.com>
2024-12-18 21:02:22 +0200

    q5

--- frontend/src/api/services/AuthService.js
+++ frontend/src/api/services/AuthService.js
@@ -1,21 +1,21 @@
 import $api from "../http/index.js";
 
 export default class AuthService {
-    // static async getCsrfToken() {
-    //     try {
-    //         await $api.get('/sanctum/csrf-cookie', { withCredentials: true });
-    //         console.log("CSRF-токен успешно получен.");
-    //     } catch (error) {
-    //         console.error("Ошибка получения CSRF-токена:", error);
-    //         throw error;
-    //     }
-    // }
+    static async getCsrfToken() {
+        try {
+            await $api.get('/sanctum/csrf-cookie', { withCredentials: true });
+            console.log("CSRF-токен успешно получен.");
+        } catch (error) {
+            console.error("Ошибка получения CSRF-токена:", error);
+            throw error;
+        }
+    }
 
     // Метод для входа в систему
     static async login(email, password) {
         try {
             // Получаем CSRF токен перед отправкой запроса
-            // await this.getCsrfToken();
+            await this.getCsrfToken();
 
             // Выполняем запрос на логин
             const response = await $api.post('/login', { 
@@ -32,7 +32,24 @@ export default class AuthService {
     }
 
     static async registration(email, password, password_confirmation) {
-        return $api.post('/api/register', { email, password,  password_confirmation});
+        try {
+            // Получаем CSRF токен перед отправкой запроса
+            await this.getCsrfToken();
+
+            // Выполняем запрос на логин
+            const response = await $api.post('/api/register', { 
+                email, 
+                password,
+                password_confirmation
+            }, {
+                withCredentials: true  // Обязательно для передачи куки
+            });
+            return response;
+        } catch (error) {
+            console.error("Ошибка при регистрации:", error);
+            throw error;
+        }
+        // return $api.post('/api/register', { email, password,  password_confirmation});
     }
 
     static async logout() {
