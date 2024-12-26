4cccd12e305b57c97a0d1a13a745290cb9ba12bb
Illia <illia.dubrovin@gmail.com>
2024-12-26 10:15:41 +0200

    p5

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,20 +15,16 @@ return [
     |
     */
 
-   'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:3000', 'http://135.181.84.236'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => true,
+    
+        'paths' => ['api/*', 'sanctum/csrf-cookie'],
+        'allowed_methods' => ['*'],
+        'allowed_origins' => ['http://135.181.84.236', 'http://localhost:3000'],
+        'allowed_origins_patterns' => [],
+        'allowed_headers' => ['*'],
+        'exposed_headers' => [],
+        'max_age' => 0,
+        'supports_credentials' => true,
+  
+    
 
 ];
