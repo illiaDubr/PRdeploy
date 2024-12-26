e65f09b6a4762d155c5d39b9b24b0633f5562418
Illia <illia.dubrovin@gmail.com>
2024-12-26 09:52:35 +0200

    asd

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000', 'http://135.181.84.236')],
 
     'allowed_origins_patterns' => [],
 
